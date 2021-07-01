50c1fc356d8cc7ec2d36497a27a82279b9f73596
Jason <jasonpsmith180@gmail.com>
2021-06-30 18:31:59 -0600

    linked added login and signup routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,11 @@ import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@ap
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import NoMatch from './pages/NoMatch';
 
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // establish link to graphql server
 const httpLink = createHttpLink({
@@ -22,15 +25,23 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      {/* <Router> */}
+      <Router>
         <div className='flex-column justify-flex-start min-100-vh'>
           <Header />
             <div className='container'>
-              <Home />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              {/* <Route exact path="/profile/:username?" component={Profile} />
+              <Route exact path="/thought/:id" component={SingleThought} /> */}
+            
+              <Route component={NoMatch} />
+            </Switch>
             </div>
           <Footer />
         </div>
-      {/* </Router> */}
+      </Router>
     </ApolloProvider>
   );
 }
